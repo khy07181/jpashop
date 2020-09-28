d34dc1f4ef756ead55adb0f550b8f2a0b16a3806
kimhayoung <qlalzl9@naver.com>
2020-09-28 20:04:53 +0900

    프로젝트 환경설정

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+}
